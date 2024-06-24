aa18a8ce7c1a2ac0ffd4475059471ae958c2c6f6
Roberto Costa <robertocosta66@gmail.com>
2024-06-24 12:02:03 -0300

    Initial project

---
+++ back-end/src/main/java/rccommerce/RccommerceApplication.java
@@ -0,0 +1,13 @@
+package rccommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RccommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RccommerceApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/rccommerce/RccommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package rccommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RccommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
