c9a59ece59392697bc4345b4cb05df42e9e2b623
Roberto Costa <robertocosta66@gmail.com>
2024-06-25 15:30:33 -0300

    Entities User, Role, H2 settings and feed

---
+++ back-end/src/main/java/rccommerce/entitie/User.java
@@ -0,0 +1,119 @@
+package rccommerce.entitie;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	@Column(unique = true)
+	private String email;
+	private LocalDate birthDate;
+	private String password;
+	private String phone;
+	
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+    		joinColumns = @JoinColumn(name = "user_id"),
+    		inverseJoinColumns = @JoinColumn(name = "role_id"))
+	Set<Role> roles = new HashSet<>();
+	
+	public User() {
+	}
+
+	public User(Long id, String name, String email, LocalDate birthDate, String password, String phone) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.password = password;
+		this.phone = phone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ back-end/src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_SELLER');
+
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '85 98888-0001','$2a$10$efAKHcdzfnBwg5yCvuiOMeiu8pB6TuvNayPjVAZpopc1Ijx95Wyu2', '2001-07-25');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Bob Green', 'bob@gmail.com', '85 97777-0002','$2a$10$efAKHcdzfnBwg5yCvuiOMeiu8pB6TuvNayPjVAZpopc1Ijx95Wyu2', '1995-01-20');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Blue', 'alex@gmail.com','85 96666-0003', '$2a$10$efAKHcdzfnBwg5yCvuiOMeiu8pB6TuvNayPjVAZpopc1Ijx95Wyu2', '2003-08-05');
+
+INSERT INTO tb_user_role(user_id, role_id) VALUES(1,1)
+INSERT INTO tb_user_role(user_id, role_id) VALUES(1,2)
+INSERT INTO tb_user_role(user_id, role_id) VALUES(1,3)
+INSERT INTO tb_user_role(user_id, role_id) VALUES(2,2)
+INSERT INTO tb_user_role(user_id, role_id) VALUES(3,3)
